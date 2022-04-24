4f45f3beb47203b1708c084a1476e6f0bd7b8d39
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-24 10:40:17 -0300

    files static

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const exphbs = require('express-handlebars')
-app.engine('handlebars', exphbs({ defaultLayout: 'layout' }))
-app.set('view engine', 'handlebars')
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }));
+app.set('view engine', 'handlebars');
 
-const router = require('./routes/index')
-app.use('/', router())
+app.use(express.static(path.join(__dirname, 'public')));
+
+const router = require('./routes/index');
+app.use('/', router());
 
 app.listen(3000);
