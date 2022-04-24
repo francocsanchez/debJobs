189c67b578d70315cf736be4a32523ef86baa32c
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-24 10:36:38 -0300

    config view engine

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-const router = require('./routes/index')
+const exphbs = require('express-handlebars')
+app.engine('handlebars', exphbs({ defaultLayout: 'layout' }))
+app.set('view engine', 'handlebars')
 
+const router = require('./routes/index')
 app.use('/', router())
 
 app.listen(3000);
