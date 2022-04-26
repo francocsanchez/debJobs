18688948ea3a5f3385dd040b9f016125a0fb52d9
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-26 14:26:36 -0300

    Create models vacantes

---
+++ models/Vacantes.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slug');
+const shortid = require('shortid');
+
+const vacantesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: 'El nombre de la vacante es requerido',
+        trim: true
+    },
+    company: {
+        type: String,
+        trim: true
+    },
+    location: {
+        type: String,
+        trim: true,
+        required: 'La ubicacion es requerida'
+    },
+    salary: {
+        type: String,
+        default: 0,
+        trim: true
+    },
+    contract: {
+        type: String,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    url: {
+        type: String,
+        lowercase: true
+    },
+    skills: [String],
+    candidates: [{
+        name: String,
+        email: String,
+        cv: String
+    }]
+});
+
+vacantesSchema.pre('save', function (next) {
+
+    const url = slug(this.url);
+    this.url = `${url}-${shortid.generate()}`
+
+    next();
+})
+
+module.exports = mongoose.model('Vacante', vacantesSchema);
