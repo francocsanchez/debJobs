b6c47ca1509ede05fa348d86db502d6504889afb
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-25 22:40:53 -0300

    Config database

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ require('./config/db');
 const express = require('express');
 const exphbs = require('express-handlebars').create({ defaultLayout: 'layout' });
 const path = require('path');
+const cookieParse = require('cookie-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+
 const app = express();
 
 require('dotenv').config({ path: 'variables.env' });
@@ -12,6 +16,14 @@ app.engine('handlebars', exphbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParse());
+app.use(session({
+    secret: process.env.SECRET,
+    key: process.env.KEY,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DATABASE }),
+}))
 
 const router = require('./routes/index');
 app.use('/', router());
