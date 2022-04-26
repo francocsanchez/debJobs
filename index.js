602dd78fe178047364f9256754463b659de485ae
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-25 22:29:07 -0300

    config mongoose

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const mongoose = require('mongoose')
+require('./config/db');
+
 const express = require('express');
 const exphbs = require('express-handlebars').create({ defaultLayout: 'layout' });
 const path = require('path');
 const app = express();
+
+require('dotenv').config({ path: 'variables.env' });
 
 app.engine('handlebars', exphbs.engine);
 app.set('view engine', 'handlebars');
@@ -11,4 +16,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 const router = require('./routes/index');
 app.use('/', router());
 
-app.listen(3000);
+app.listen(process.env.PORT);
