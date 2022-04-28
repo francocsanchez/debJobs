1f97b125d57e3e7f660f6730f37350262c9c471a
Franco C Sanchez <francocsanchez@gmail.com>
2022-04-27 21:42:12 -0300

    find vacantes

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,8 +1,14 @@
-exports.listJobs = (req, res) => {
+const mongoose = require('mongoose');
+const Vacante = mongoose.model('Vacante');
+
+exports.listJobs = async (req, res) => {
+    const vacantes = await Vacante.find({}).lean();
+    console.log(vacantes);
     res.render('home', {
         nombrePagina: 'DevJobs',
         tagline: 'Encuentra tu proximo trabajo como desarrollador',
         barra: true,
-        boton: true
+        boton: true,
+        vacantes
     })
 }
